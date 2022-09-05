b3ff3d46e363c83d2c22e9ffe30fadd8b70490b8
Arnold <arnold.m.dsilva@gmail.com>
2022-09-05 18:54:27 +1000

    Bug Fix.

---
+++ js/helpers.js
@@ -0,0 +1,7 @@
+// To be worked on
+function fadeInAndOut(p1, p2) {
+    var p1 = document.getElementById(p1);
+    p1.style.display = "none";
+    var p2 = document.getElementById(p2);
+    p2.style.display = "block";
+}
